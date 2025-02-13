0ce5dfe768984c573cf84780a24c5faf85a989e1
R-unic <roleyspersonal@gmail.com>
2025-02-12 19:51:03 -0500

    feat: make sizeOf() for types not values

--- main.cpp
+++ main.cpp
@@ -1,114 +1,103 @@
+#include <unordered_map>
+
 #include "./common.hpp"
 
-void process_exit()
+static void process_exit()
 {
     exit(EXIT_FAILURE);
 }
 
-int get_memory_address(lua_State* L)
+static int get_memory_address(lua_State* L)
 {
     const void* ptr = lua_topointer(L, 1);
-    uintptr_t ptrValue = reinterpret_cast<uintptr_t>(ptr);
-    lua_pushinteger(L, static_cast<lua_Integer>(ptrValue));
+    const uintptr_t value = reinterpret_cast<uintptr_t>(ptr);
+    lua_pushinteger(L, value);
+    
     return 1;
 }
 
-int size_of(lua_State* L)
-{
-    auto argType = lua_type(L, 1);
+static std::unordered_map<std::string, int> type_sizes = {
+    {"number", 8},
+    {"boolean", 1},
+    {"string", 4},
+    {"userdata", 4},
+    {"table", 4},
+    {"function", 4},
+    {"thread", 4}
+};
 
-    if (argType == LUA_TNUMBER)
-    {
-        lua_pushinteger(L, 8); // cause all numbers are 64-bit in lua
+static int size_of(lua_State* L)
+{
+    const char* type_name = lua_tostring(L, -1);
+    for (const auto& [key, value] : type_sizes) {
+        if (std::strcmp(key.c_str(), type_name) != 0) continue;
+        
+        lua_pushinteger(L, value); // cause all numbers are 64-bit in lua
         return 1;
     }
-    else if (argType == LUA_TBOOLEAN)
-    {
-        lua_pushinteger(L, 1); // 1 byte for a boolean, weird
-        return 1;
-    }
-    else if (argType == LUA_TSTRING || argType == LUA_TTABLE || argType == LUA_TFUNCTION || argType == LUA_TTABLE || argType == LUA_TLIGHTUSERDATA || argType == LUA_TTHREAD)
-    {
-        lua_pushinteger(L, 4); // all pointers internally
-        return 1;
-    }
-    else if (argType == LUA_TUSERDATA)
-    {
-        auto length = lua_rawlen(L, 1);
-        lua_pushinteger(L, length);
-        return 1;
-    }
-    else if (argType == LUA_TNIL)
-    {
-        lua_pushinteger(L, 0);
-        return 1;
-    }
-    else
-    {
-        return luaL_error(L, "unsupported sizeOf value");
-    }
+    
+    return luaL_error(L, "sizeOf expects a type string, got value %s", type_name);
 }
 
 class LowLua
 {
-private:
     lua_State* L;
     int argc;
     char** argv;
 
 public:
-    LowLua(int argc, char** argv)
+    LowLua(const int argc, char** argv)
     {
         this->L = luaL_newstate();
         this->argc = argc;
         this->argv = argv;
     }
 
-    void do_file(std::string filePath)
+    void do_file(const std::string& file_path)
     {
-        if (luaL_dofile(this->L, filePath.c_str()) != LUA_OK)
+        if (luaL_dofile(this->L, file_path.c_str()) != LUA_OK)
             this->error();
     }
 
-    void do_string(std::string str)
+    void do_string(const std::string& str)
     {
         if (luaL_dostring(this->L, str.c_str()) != LUA_OK)
             this->error();
     }
 
-    void open_libs()
+    void open_custom_libs() const
     {
         lua_register(this->L, "getMemoryAddress", get_memory_address);
         lua_register(this->L, "sizeOf", size_of);
     }
 
-    void open_default_libs()
+    void open_default_libs() const
     {
         luaL_openlibs(this->L);
     }
 
-    void close()
+    void close() const
     {
         lua_close(this->L);
     }
 
-    void error(std::string msg = "")
+    void error(const std::string& msg = "")
     {
         if (msg.empty())
             puts(lua_tostring(this->L, this->top()));
         else
-            puts(("[Luay] " + msg).c_str());
+            puts(("[LowLua] " + msg).c_str());
 
         this->pop_top();
         process_exit();
     }
 
-    int top()
+    int top() const
     {
         return lua_gettop(this->L);
     }
 
-    void pop_top()
+    void pop_top() const
     {
         lua_pop(this->L, this->top());
     }
@@ -123,14 +112,12 @@ int main(int argc, char** argv)
         puts("Usage: lowlua <file>\n");
         return EXIT_FAILURE;
     }
-    else
-    {
-        char* fileDir = argv[1];
-        lua.open_default_libs();
-        lua.open_libs();
-        lua.do_file(fileDir);
-    }
 
+    const char* file_directory = argv[1];
+    lua.open_default_libs();
+    lua.open_custom_libs();
+    lua.do_file(file_directory);
     lua.close();
+    
     return EXIT_SUCCESS;
 }
